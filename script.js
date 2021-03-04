7d35796610eaf954d0df55ded69e19135c96b68f
schornb <55115049+schornb@users.noreply.github.com>
2021-03-03 19:39:52 -0500

    change in ordering of text

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function carousel() {
 }
 
 // Set the date we're counting down to
-var countDownDate = new Date("Jan 5, 2022 15:37:25").getTime();
+var countDownDate = new Date("February 20, 2022 15:37:25").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {
